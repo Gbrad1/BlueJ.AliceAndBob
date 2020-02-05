3ae0e8e023d33fa7fa102e2a394631ca22f08c0f
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-05 15:08:04 -0500

    I have gone over the test cases and determined that the purpose of this program is to look for Alice or Bob.

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -1,22 +1,28 @@
-
 /**
- * Write a description of class AliceAndBobEngine here.
+ * The purpse of this program is to have the test
+ case recognize if they know who Alice and Bob is.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Giles Bradford
+ * @version 1.0 5-Feb-2020
  */
 public class AliceAndBobEngine
 {
     String result;
-    
+
     AliceAndBobEngine(){
         result = "";
     }
 
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
-    public String evaluateAliceOrBob(String input)
-    {
-    
-        return result;
-    }
+    public String evaluateAliceOrBob(String input) {
+      if (input.equals("Alice")) {
+          return "Hello Alice";
+      } else if (input.equals("Alicia") || input.equals("Bobert")) {
+          return "I DON'T KNOW YOU, YOU ARE SCARY";
+      } else if (input.equals("Bob")) {
+          return "Hello Bob";
+      } else {
+        return "invalid";
+      }
+   }
 }
--- AliceAndBobEngineTest.java
+++ AliceAndBobEngineTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 /**
  * The test class AliceAndBobEngineTest.
  *
- * @author  (your name)
- * @version (a version number or a date)
+ * @author  Giles Bradford
+ * @version 1.0 5-Feb-2020
  */
 public class AliceAndBobEngineTest
 {
@@ -22,42 +22,42 @@ public class AliceAndBobEngineTest
     public void setUp()
     {
     }
-    
+
     @After
     public void tearDown()
     {
     }
-    
+
     @Test
     public void evaluateAliceOrBobTestAlice(){
         AliceAndBobEngine aliceAndBobEngineTest = new AliceAndBobEngine();
-        
+
         assertEquals("Hello Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Alice"));
         assertNotEquals("Hello Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Jeff. Jeff the Elephant."));
-        
+
         assertEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Alicia"));
         assertNotEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Alice"));
-        
+
     }
-    
+
     @Test
     public void evaluateAliceOrBobTestBob(){
         AliceAndBobEngine aliceAndBobEngineTest = new AliceAndBobEngine();
-        
+
         assertEquals("Hello Bob", aliceAndBobEngineTest.evaluateAliceOrBob("Bob"));
         assertNotEquals("Hello Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Pamela. Pamela the tropical citrus tree."));
-        
+
         assertEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Bobert"));
         assertNotEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Bob"));
     }
-    
+
     @Test
     public void evaluateAliceOrBobTestScary(){
         AliceAndBobEngine aliceAndBobEngineTest = new AliceAndBobEngine();
-        
+
         assertNotEquals("I don't know you, you are scary?", aliceAndBobEngineTest.evaluateAliceOrBob("Cynthia"));
         assertNotEquals("I don't know you, you are scary!", aliceAndBobEngineTest.evaluateAliceOrBob("Simba"));
         assertNotEquals("New method, who dis?", aliceAndBobEngineTest.evaluateAliceOrBob("Luna"));
     }
-        
+
 }
